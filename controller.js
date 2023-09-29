aa45181359ed80eb9b5f7d149af5bececd393c10
Nicolly Silva <nicolly.avila@alunoescolassociais.org.br>
2023-09-29 11:27:59 -0300

    Limpar o formulario, verificar o tamanho do cep e da validaçao

--- controller.js
+++ controller.js
@@ -9,3 +9,7 @@ const limparFormulario = () => {
     document.getElementById('Cidade').value = '';
     document.getElementById('Estado').value = '';
 }
+
+// Verificar se CEP e valido
+const eNumero = (numero) => /^[0-9]+$/.teste(numero); //testa numero informado com expressao regular
+const cepValido = (cep) => cep.lenght == 8 && eNumero(cep); // verifica tamanho do cep digitado e executa funçao de validaçao do cep eNumero 
