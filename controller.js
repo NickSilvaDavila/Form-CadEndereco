1b6c8d2b548e9ceee864a427af9aa0c28ddcfcad
Nicolly Silva <nicolly.avila@alunoescolassociais.org.br>
2023-09-29 11:09:12 -0300

    comitar

--- controller.js
+++ controller.js
@@ -1,1 +1,11 @@
 'use strict'; // Modo restrito
+//Consumindo API de CEP, do ViaCep
+// https://viacep.com.br/
+
+//Funçao para limpar o formulario
+const limparFormulario = () => {
+    document.getElementById('Rua').value = '';
+    document.getElementById('Bairro').value = '';
+    document.getElementById('Cidade').value = '';
+    document.getElementById('Estado').value = '';
+}
